53271bd8bd916401a2c3ec334e2218032ec402a7
Hannes Rabo <hannes.rabo@gmail.com>
2019-02-22 21:42:15 +0100

    feat(page): Updated the projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
 import Page from '../components/layout/Page'
+import { Card } from '../components/Card'
+import styled from 'styled-components'
+
+const ExperienceCardContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+`
+
+const CardContainer = styled.div`
+	flex-grow: 3;
+	margin-left: 30px;
+`
 
 class projects extends Component {
 	render() {
 		return (
 			<Page currentPage="/projects">
-				<div>
-					<p>Projects.</p>
-				</div>
+				<ExperienceCardContainer>
+					<p>This is some info</p>
+
+					<CardContainer>
+						<Card>
+							<p>This is some experience</p>
+						</Card>
+						<Card>
+							<p>This is some experience</p>
+						</Card>
+					</CardContainer>
+				</ExperienceCardContainer>
 			</Page>
 		)
 	}
